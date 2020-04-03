ddb1207a3a58968ab4412a0b1aa2964bdeaa5679
krus <thecrusheda@gmail.com>
2020-04-03 19:44:17 +0700

    add some check pullgit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // unchecklist after used
+
         // $this->call(AdminTableSeeder::class);
         // $this->call(OtherRoleTableSeeder::class);
         // $this->call(DirekturTableSeeder::class);
